b09fee38a17fc61f319982145c93d154a4af1874
unknown <you@example.com>
2026-04-01 17:59:59 -0300

    colocar cor com js

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-import {calcularImc, definirClassificacao} from './opc.js'
+import { calcularImc, definirClassificacao } from "./opc.js";
 
-let trs = document.querySelectorAll('tbody tr')
+let trs = document.querySelectorAll("tbody tr");
 
 // laço de repetição
-for (let index = 0; index < trs.length; index++)
- {
-    let tr = trs[index]
-    let filhos = tr.children
-    
-    // notas
-    let idade = filhos[1].textContent
-    let altura= filhos[2].textContent
-    let peso = filhos[3].textContent
+for (let index = 0; index < trs.length; index++) {
+  let tr = trs[index];
+  let filhos = tr.children;
 
-    idade = parseFloat(idade)
-    altura = parseFloat(altura)
-    peso = parseFloat(peso)
+  // notas
+  let idade = filhos[1].textContent;
+  let altura = filhos[2].textContent;
+  let peso = filhos[3].textContent;
 
-      // cálculo do IMC
+  idade = parseFloat(idade);
+  altura = parseFloat(altura);
+  peso = parseFloat(peso);
 
-  let resultado = calcularImc(peso, altura)
-    filhos[4].textContent = resultado.toFixed(1)
+  // cálculo do IMC
 
+  let resultado = calcularImc(peso, altura);
+  filhos[4].textContent = resultado.toFixed(1);
 
-    // classificação
+  // classificação
 
-    filhos[5].textContent = definirClassificacao(resultado)
+  filhos[5].textContent = definirClassificacao(resultado);
 }
 
-   
+// Cor
+trs.forEach((tr) => {
+  let filhos = tr.children;
 
+  let status = filhos[5];
 
-
- 
+  if (status.textContent == "Peso normal") {
+    status.style.backgroundColor = "#F4EFEC";
+  } else if (status.textContent == "Sobrepeso") {
+    status.style.backgroundColor = "#edf4d8";
+  } else if (status.textContent == "Obesidade Grau I") {
+    status.style.backgroundColor = "#cb9c9c";
+  }
+});
